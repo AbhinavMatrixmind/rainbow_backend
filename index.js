482bf83ec85fd24bdfebe1f3ab84b7d3890b1492
AbhinavMatrixmind <164630776+AbhinavMatrixmind@users.noreply.github.com>
2024-03-23 18:01:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const cors = require("cors");
+const { google } = require("googleapis");
+
+const app = express();
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
+const getGoogleSheetsData = async (range) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  const client = await auth.getClient();
+
+  const googleSheets = google.sheets({
+    version: "v4",
+    auth: client,
+  });
+
+  const spreadsheetId = "1dqgEz77W4Z2DEsD5vRB24lDA128ds-G8IjoCfavauKY";
+
+  const data = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range,
+  });
+
+  return data.data;
+};
+
+app.get("/api/manpower", async (req, res) => {
+  try {
+    const data = await getGoogleSheetsData("Manpower");
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching data from Google Sheets:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/license", async (req, res) => {
+  try {
+    const data = await getGoogleSheetsData("Licenses");
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching data from Google Sheets:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/lap_instrument", async (req, res) => {
+  try {
+    const data = await getGoogleSheetsData("Lap Instrument");
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching data from Google Sheets:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/general_instrument", async (req, res) => {
+  try {
+    const data = await getGoogleSheetsData("General Instrument List");
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching data from Google Sheets:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/general_list", async (req, res) => {
+  try {
+    const data = await getGoogleSheetsData("General List");
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching data from Google Sheets:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/ward_wise_stock", async (req, res) => {
+  try {
+    const data = await getGoogleSheetsData("ward_wise_stock");
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching data from Google Sheets:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/capexMedical", async (req, res) => {
+  try {
+    const data = await getGoogleSheetsData("Equipments");
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching data from Google Sheets:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/api/infrastructure", async (req, res) => {
+  try {
+    const data = await getGoogleSheetsData("infrastructure");
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching data from Google Sheets:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(1337, (req, res) => {
+  console.log("running on 1337");
+});
